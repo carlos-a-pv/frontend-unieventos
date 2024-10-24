b1d9c07bf1c306f8b5eb35842661880b86435b65
carlos.perez <carlospverano@gmail.com>
2024-10-23 19:17:33 -0500

    interfaz login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegistroComponent},
+    {path: "**", pathMatch: "full", redirectTo: ""}
+];
