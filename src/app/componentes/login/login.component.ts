261b7dd34b9bc869e4a8c90f9e9a2c99c6b88113
carlos.perez <carlospverano@gmail.com>
2024-10-25 11:17:08 -0500

    Header terminado

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  title = 'UniEventos';
 }
