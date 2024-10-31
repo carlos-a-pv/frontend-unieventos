a57c4dd2b4fafcd8c0e3bc85bf8f066570510f74
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-10-30 19:33:47 -0500

    creacion del componente registro evento

---
+++ src/app/componentes/registro-evento/registro-evento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-evento',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-evento.component.html',
+  styleUrl: './registro-evento.component.css'
+})
+export class RegistroEventoComponent {
+
+}
