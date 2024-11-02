0f4573bfd87a6be74b0537f0296f239fc93d1ce5
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-01 21:33:53 -0500

    algo

--- src/app/componentes/registro-evento/registro-evento.component.ts
+++ src/app/componentes/registro-evento/registro-evento.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-evento',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro-evento.component.html',
   styleUrl: './registro-evento.component.css'
 })
 export class RegistroEventoComponent {
+  registroEventoForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  private crearFormularioEvento(){
+  }
 
 }
