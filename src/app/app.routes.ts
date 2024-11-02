d080f8a0e0bb6f469ed6b32744468ffdc78bd5fe
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-01 21:30:31 -0500

    Registro Evento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
+import { RegistroEventoComponent } from './componentes/registro-evento/registro-evento.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
+    {path: 'registro-evento', component: RegistroEventoComponent},
     {path: "**", pathMatch: "full", redirectTo: ""}
 ];
