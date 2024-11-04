0da9195cf7720a0a6479f9d5ec3915506517e967
carlos.perez <carlospverano@gmail.com>
2024-11-04 00:44:58 -0500

    archivo de rutas actulizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
+import { RecuperarCuentaComponent } from './componentes/recuperar-cuenta/recuperar-cuenta.component';
+import { ValidarCodigoRecuperacionComponent } from './componentes/validar-codigo-recuperacion/validar-codigo-recuperacion.component';
+import { ActivarCuentaComponent } from './componentes/activar-cuenta/activar-cuenta.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'registro', component: RegistroComponent},
+    {path: 'recuperar-cuenta', component: RecuperarCuentaComponent},
+    {path: 'validar-codigo-recuperacion', component: ValidarCodigoRecuperacionComponent},
+    {path: 'activar-cuenta', component: ActivarCuentaComponent},
     {path: "**", pathMatch: "full", redirectTo: ""}
 ];
