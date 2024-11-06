afa485a81f0cf4dd8e95eb3792622c0a2a1d5ef7
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-05 20:16:17 -0500

    cambios de brahian, logica de login, registro de evento

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BackgroundComponent } from "../background/background.component";
 
@@ -10,6 +11,20 @@ import { BackgroundComponent } from "../background/background.component";
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
   title = 'UniEventos';
+  loginForm!: FormGroup;
+
+  constructor(private formBuider: FormBuilder){
+    this.crearFormularioLogin();
+  }
+  private crearFormularioLogin(){
+    this.loginForm = this.formBuider.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]]
+     })
+  }
+  public ingresar(){
+    console.log(this.loginForm.value)
+  }
+
 }
