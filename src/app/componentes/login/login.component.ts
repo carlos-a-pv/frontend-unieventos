48e2d0fbb101287ceb6b7edad8c3bca7951a1dd1
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-05 20:16:17 -0500

    cambios de brahian, logica de login, registro de evento

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,6 +10,20 @@ import { RouterModule } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
   title = 'UniEventos';
+  loginForm!: FormGroup;
+
+  constructor(private formBuider: FormBuilder){
+    this.crearFormularioLogin();
+  }
+  private crearFormularioLogin(){
+    this.loginForm = this.formBuider.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]]
+     })
+  }
+  public ingresar(){
+    console.log(this.loginForm.value)
+  }
+
 }
--- src/app/componentes/registro-evento/registro-evento.component.ts
+++ src/app/componentes/registro-evento/registro-evento.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-evento',
@@ -9,11 +9,50 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './registro-evento.component.css'
 })
 export class RegistroEventoComponent {
-  registroEventoForm!: FormGroup;
+  crearEventoForm!: FormGroup;
+  tiposDeEvento: string[];
 
-  constructor(private formBuilder: FormBuilder){ }
+
+  constructor(private formBuilder: FormBuilder){ 
+    this.crearFormularioEvento();
+    this.tiposDeEvento = ['Concierto', 'Fiesta', 'Teatro', 'Deportes'];
+  }
 
   private crearFormularioEvento(){
+    this.crearEventoForm = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      fecha: ['', [Validators.required]],
+      tipoEvento: ['', [Validators.required]],
+      direccion: ['', [Validators.required]],
+      ciudad: ['', [Validators.required]],
+      localidades: this.formBuilder.array([]),
+      imagenPortada: ['', [Validators.required]],
+      imagenLocalidades: ['', [Validators.required]]
+    });
+   
   }
+  public crearEvento(){
+    console.log(this.crearEventoForm.value);
+   }
+   
+  public onFileChange(event:any, tipo:string){
+    if (event.target.files.length > 0) {
+      const files = event.target.files;     
+   
+   
+      switch(tipo){
+        case 'localidades':
+          this.crearEventoForm.get('imagenLocalidades')?.setValue(files[0]);
+          break;
+        case 'portada':
+          this.crearEventoForm.get('imagenPortada')?.setValue(files[0]);
+          break;
+      }
+   
+   
+    }
+   }
+   
 
 }
