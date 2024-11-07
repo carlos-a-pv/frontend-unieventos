510ee5d2fec63c1b234f498a33385f41cbde2d85
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-06 19:22:40 -0500

    cambios minimos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { RecuperarCuentaComponent } from './componentes/recuperar-cuenta/recuper
 import { ValidarCodigoRecuperacionComponent } from './componentes/validar-codigo-recuperacion/validar-codigo-recuperacion.component';
 import { ActivarCuentaComponent } from './componentes/activar-cuenta/activar-cuenta.component';
 import { RegistroEventoComponent } from './componentes/registro-evento/registro-evento.component';
-import { RegistroEventoComponent } from './componentes/registro-evento/registro-evento.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -15,7 +14,6 @@ export const routes: Routes = [
     {path: 'recuperar-cuenta', component: RecuperarCuentaComponent},
     {path: 'validar-codigo-recuperacion', component: ValidarCodigoRecuperacionComponent},
     {path: 'activar-cuenta', component: ActivarCuentaComponent},
-    {path: 'registro-evento', component: RegistroEventoComponent},
     {path: 'registro-evento', component: RegistroEventoComponent},
     {path: "**", pathMatch: "full", redirectTo: ""}
 ];
