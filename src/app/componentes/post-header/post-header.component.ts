24f7c8840a590bb7b0e2ec9c23e50087ec84e030
carlos.perez <carlospverano@gmail.com>
2024-11-09 01:16:33 -0500

    separacion de componentes

---
+++ src/app/componentes/post-header/post-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-header.component.html',
+  styleUrl: './post-header.component.css'
+})
+export class PostHeaderComponent {
+
+}
