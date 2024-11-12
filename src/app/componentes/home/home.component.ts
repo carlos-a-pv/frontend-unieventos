2b492d431d853d04a750ccd68812e982ac0af39e
carlos.perez <carlospverano@gmail.com>
2024-11-11 21:21:24 -0500

    componente evento terminado

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -3,11 +3,12 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { PostHeaderComponent } from "../post-header/post-header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { EventoComponent } from "../evento/evento.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HeaderComponent, PostHeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, HeaderComponent, PostHeaderComponent, FooterComponent, EventoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
