7f26977f3ec644eec6b796f34e4e3bcaebc33bd7
brahian <brahiana.arbelaeza@uqvirtual.edu.co>
2024-11-16 00:17:10 -0500

    Gestion de Eventos Avance

--- src/app/componentes/detalle-evento/detalle-evento.component.ts
+++ src/app/componentes/detalle-evento/detalle-evento.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EventosService } from '../../servicios/eventos.service';
+import { CommonModule } from '@angular/common';
+import { InformacionEventoDTO } from '../../dto/evento/informacion-evento-dto';
+
 
 @Component({
-  selector: 'app-detalle-evento',
-  standalone: true,
-  imports: [],
-  templateUrl: './detalle-evento.component.html',
-  styleUrl: './detalle-evento.component.css'
+ selector: 'app-detalle-evento',
+ standalone: true,
+ imports: [CommonModule],
+ templateUrl: './detalle-evento.component.html',
+ styleUrl: './detalle-evento.component.css'
 })
 export class DetalleEventoComponent {
 
+
+ codigoEvento: string = '';
+ evento: InformacionEventoDTO | undefined;
+
+
+ constructor(private route: ActivatedRoute, private eventosService: EventosService) {
+   this.route.params.subscribe((params) => {
+     this.codigoEvento = params['id'];
+     this.obtenerEvento();
+   });
+ }
+
+
+ public obtenerEvento() {
+   const eventoConsultado = this.eventosService.obtener(this.codigoEvento);
+   if (eventoConsultado != undefined) {
+     this.evento = eventoConsultado;
+   }
+ }
+
+
 }
+
